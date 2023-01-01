624a39b0f5795f351d621b7ac3caefadf7005d65
parkjiye <parkji3618@naver.com>
2023-01-01 20:47:27 +0900

    [CHORE] 개발 환경 세팅 (#1)

---
+++ src/main/java/com/skku/bloodline/BloodlineApplication.java
@@ -0,0 +1,13 @@
+package com.skku.bloodline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skku/bloodline/BloodlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skku.bloodline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
