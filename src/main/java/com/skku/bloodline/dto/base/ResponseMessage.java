99f437f4a241f8b3b35931019b28cb41f19a7a6c
parkjiye <parkji3618@naver.com>
2023-02-03 13:44:43 +0900

    [FEAT] Response Message 추가 (#4)

--- src/main/java/com/skku/bloodline/dto/base/ResponseMessage.java
+++ src/main/java/com/skku/bloodline/dto/base/ResponseMessage.java
@@ -5,5 +5,7 @@ public class ResponseMessage {
   public static final String POST_CONCERT = "POST concert success";
   public static final String POST_APPLYREQUEST = "POST Apply Request success";
   public static final String POST_SONG = "POST song success";
-  public static final String GET_SONG = "Get songs success";
+  public static final String GET_SONG = "GET songs success";
+  public static final String POST_TEAM = "POST teams success";
+  public static final String GET_TEAM = "GET teams success";
 }
