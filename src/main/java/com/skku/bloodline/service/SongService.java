93e99457604df0104cb492c7ea55fa050c47d6b3
parkjiye <parkji3618@naver.com>
2023-02-03 10:57:14 +0900

    [FEAT] saveSong service method 추가 (#10)

---
+++ src/main/java/com/skku/bloodline/service/SongService.java
@@ -0,0 +1,8 @@
+package com.skku.bloodline.service;
+
+import com.skku.bloodline.dto.request.CreateSongDTO;
+import com.skku.bloodline.dto.response.SongResponseDTO;
+import java.util.List;
+
+public interface SongService {
+  Long saveSong(CreateSongDTO createSongDTO);
---
+++ src/main/java/com/skku/bloodline/service/SongServiceImpl.java
@@ -0,0 +1,24 @@
+package com.skku.bloodline.service;
+
+import com.skku.bloodline.domain.Song;
+import com.skku.bloodline.dto.request.CreateSongDTO;
+import com.skku.bloodline.dto.response.SongResponseDTO;
+import com.skku.bloodline.repository.SongRepository;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SongServiceImpl implements SongService {
+  private final SongRepository songRepository;
+
+  public SongServiceImpl(SongRepository songRepository) {
+    this.songRepository = songRepository;
+  }
+
+  @Transactional
+  public Long saveSong(CreateSongDTO createSongDTO) {
+    return songRepository.save(createSongDTO.toEntity()).getId();
+  }
+
