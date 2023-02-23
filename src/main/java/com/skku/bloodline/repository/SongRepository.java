496b4f5ac7ca0856174b1d37c31348f450aee464
parkjiye <parkji3618@naver.com>
2023-02-23 16:41:08 +0900

    [FEAT] SongRepository 구성

--- src/main/java/com/skku/bloodline/repository/SongRepository.java
+++ src/main/java/com/skku/bloodline/repository/SongRepository.java
@@ -1,6 +1,13 @@
 package com.skku.bloodline.repository;
 
 import com.skku.bloodline.domain.Song;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface SongRepository extends JpaRepository<Song, Long> {}
+public interface SongRepository extends JpaRepository<Song, Long> {
+
+  @Query("SELECT a FROM song a " + "WHERE a.concert = :concert")
+  List<Song> findByConcert(@Param("concert") String concert);
+}
