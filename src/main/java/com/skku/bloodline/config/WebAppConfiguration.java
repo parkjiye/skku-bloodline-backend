45e4142fdbb7ab6bf276462bfb05204123ba59bd
parkjiye <parkji3618@naver.com>
2023-02-23 16:43:44 +0900

    [FIX] CORS Error 수정 추가

--- src/main/java/com/skku/bloodline/config/WebAppConfiguration.java
+++ src/main/java/com/skku/bloodline/config/WebAppConfiguration.java
@@ -1,8 +1,10 @@
 package com.skku.bloodline.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebAppConfiguration implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
