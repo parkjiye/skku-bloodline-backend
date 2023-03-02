5db8a9bde8a78b4f58c4d7ac1dcf9d7ed97b8f2e
parkjiye <parkji3618@naver.com>
2023-03-02 18:34:08 +0900

    [FEAT] findByConcert repository method 생성

--- src/main/java/com/skku/bloodline/repository/TeamRepository.java
+++ src/main/java/com/skku/bloodline/repository/TeamRepository.java
@@ -1,6 +1,12 @@
 package com.skku.bloodline.repository;
 
 import com.skku.bloodline.domain.Team;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface TeamRepository extends JpaRepository<Team, Long> {}
+public interface TeamRepository extends JpaRepository<Team, Long> {
+  @Query("SELECT a FROM team a " + "WHERE a.concert_id = :concert_id")
+  List<Team> findByConcert(@Param("concert_id") Long concert_id);
+}
