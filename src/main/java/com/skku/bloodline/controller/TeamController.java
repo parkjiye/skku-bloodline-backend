5009f46fd40c6e12e01e25caad06a5090db77f61
parkjiye <parkji3618@naver.com>
2023-03-02 18:30:15 +0900

    [FEAT] concert_id로 팀 목록 조회 API 생성 (#11)

--- src/main/java/com/skku/bloodline/controller/TeamController.java
+++ src/main/java/com/skku/bloodline/controller/TeamController.java
@@ -7,9 +7,9 @@ import com.skku.bloodline.service.TeamService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -28,18 +28,19 @@ public class TeamController {
         HttpStatus.OK);
   }
 
-  @GetMapping("/api/teams")
-  public ResponseEntity<?> getTeams() {
-    return new ResponseEntity<>(
-        BaseResponse.response(HttpStatus.OK, ResponseMessage.GET_TEAM, teamService.findAllTeams()),
-        HttpStatus.OK);
-  }
+  //  @GetMapping("/api/teams")
+  //  public ResponseEntity<?> getTeams() {
+  //    return new ResponseEntity<>(
+  //        BaseResponse.response(HttpStatus.OK, ResponseMessage.GET_TEAM,
+  // teamService.findAllTeams()),
+  //        HttpStatus.OK);
+  //  }
 
-  @GetMapping("/api/teams/{teamUUID}")
-  public ResponseEntity<?> getTeam(@PathVariable("teamUUID") Long teamUUID) {
+  @GetMapping("/api/teams")
+  public ResponseEntity<?> getTeam(@RequestParam(value = "concert_id") Long concert_id) {
     return new ResponseEntity<>(
         BaseResponse.response(
-            HttpStatus.OK, ResponseMessage.GET_TEAM, teamService.findById(teamUUID)),
+            HttpStatus.OK, ResponseMessage.GET_TEAM, teamService.findByConcert(concert_id)),
         HttpStatus.OK);
   }
 }
