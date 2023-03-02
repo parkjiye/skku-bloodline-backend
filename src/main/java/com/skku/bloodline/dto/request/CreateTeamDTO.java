c2f6f7e7dd56749837f556b73dc28acb33eb0e8c
parkjiye <parkji3618@naver.com>
2023-03-02 18:24:19 +0900

    [FIX] CreateTeamDTO 수정 (#5)

--- src/main/java/com/skku/bloodline/dto/request/CreateTeamDTO.java
+++ src/main/java/com/skku/bloodline/dto/request/CreateTeamDTO.java
@@ -8,18 +8,25 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class CreateTeamDTO {
-  public String teamName;
+  public String name;
   public String description;
   public String image;
+  public Long concert_id;
 
-  @Builder
-  public CreateTeamDTO(String teamName, String description, String image) {
-    this.teamName = teamName;
+  public CreateTeamDTO(String name, String description, String image, Long concert_id) {
+    this.name = name;
     this.description = description;
     this.image = image;
+    this.concert_id = concert_id;
   }
 
+  @Builder
   public Team toEntity() {
-    return Team.builder().teamName(teamName).description(description).image(image).build();
+    return Team.builder()
+        .name(name)
+        .description(description)
+        .image(image)
+        .concert_id(concert_id)
+        .build();
   }
 }
