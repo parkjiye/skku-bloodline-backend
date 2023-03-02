c883541fd98fdbe1b85fa22f6a276cb1f49aba18
parkjiye <parkji3618@naver.com>
2023-03-02 18:22:59 +0900

    [FIX] Team 엔티티 수정 (#3)

--- src/main/java/com/skku/bloodline/domain/Team.java
+++ src/main/java/com/skku/bloodline/domain/Team.java
@@ -12,30 +12,33 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-@Entity
+@Entity(name = "team")
 public class Team {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column private String teamName;
+  @Column private String name;
   @Column private String description;
   @Column private String image;
+  @Column private Long concert_id;
 
   @Builder
-  public Team(Long id, String teamName, String description, String image) {
+  public Team(Long id, String name, String description, String image, Long concert_id) {
     this.id = id;
-    this.teamName = teamName;
+    this.name = name;
     this.description = description;
     this.image = image;
+    this.concert_id = concert_id;
   }
 
   public TeamResponseDTO toEntity() {
     return TeamResponseDTO.builder()
         .id(id)
-        .teamName(teamName)
+        .name(name)
         .description(description)
         .image(image)
+        .concert_id(concert_id)
         .build();
   }
 }
